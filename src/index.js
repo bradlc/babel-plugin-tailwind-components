4a5b5a23f64ecd9d2d63662b07909a7b98d9c70d
Brad Cornes <bradlc41@gmail.com>
2018-10-06 18:25:00 +0100

    add option to enable/disable transforms

--- src/index.js
+++ src/index.js
@@ -1,44 +1,57 @@
 const nodePath = require('path')
 import visit from './visit'
 
-export default function({ types: t }) {
+export default function({
+  types: t,
+  transformTaggedTemplateExpression = true,
+  transformJSXAttribute = false
+}) {
   let cloneNode = t.cloneNode || t.cloneDeep
 
-  return {
-    visitor: {
-      TaggedTemplateExpression(path, state) {
-        if (path.node.tag.name !== 'tw') return
+  function TaggedTemplateExpression(path, state) {
+    if (path.node.tag.name !== 'tw') return
 
-        const sourceRoot = state.file.opts.sourceRoot || '.'
-        const configPath = nodePath.resolve(
-          sourceRoot,
-          state.opts.config || './tailwind.js'
-        )
+    let sourceRoot = state.file.opts.sourceRoot || '.'
+    let configPath = nodePath.resolve(
+      sourceRoot,
+      state.opts.config || './tailwind.js'
+    )
 
-        visit({ path, configPath, t, outputFormat: state.opts.format })
-      },
-      JSXAttribute(path, state) {
-        if (path.node.name.name !== 'tw') return
-
-        const sourceRoot = state.file.opts.sourceRoot || '.'
-        const configPath = nodePath.resolve(
-          sourceRoot,
-          state.opts.config || './tailwind.js'
-        )
-
-        if (path.node.value.type === 'StringLiteral') {
-          path
-            .get('value')
-            .replaceWith(t.jSXExpressionContainer(cloneNode(path.node.value)))
-        }
-
-        path.traverse({
-          StringLiteral(path) {
-            visit({ path, configPath, t, outputFormat: state.opts.format })
-            path.skip()
-          }
-        })
-      }
-    }
+    visit({ path, configPath, t, outputFormat: state.opts.format })
   }
+
+  function JSXAttribute(path, state) {
+    if (path.node.name.name !== 'tw') return
+
+    let sourceRoot = state.file.opts.sourceRoot || '.'
+    let configPath = nodePath.resolve(
+      sourceRoot,
+      state.opts.config || './tailwind.js'
+    )
+
+    if (path.node.value.type === 'StringLiteral') {
+      path
+        .get('value')
+        .replaceWith(t.jSXExpressionContainer(cloneNode(path.node.value)))
+    }
+
+    path.traverse({
+      StringLiteral(path) {
+        visit({ path, configPath, t, outputFormat: state.opts.format })
+        path.skip()
+      }
+    })
+  }
+
+  let visitor = {}
+
+  if (transformTaggedTemplateExpression) {
+    visitor.TaggedTemplateExpression = TaggedTemplateExpression
+  }
+
+  if (transformJSXAttribute) {
+    visitor.JSXAttribute = JSXAttribute
+  }
+
+  return { visitor }
 }
